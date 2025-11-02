0132792421efab26356207fbe26c365dbf994938
flyluk <fly.luk@gmail.com>
2025-11-02 13:25:41 +0000

    bugfix for grep docker compose and run compose with profile

--- setup-rag-pipeline.sh
+++ setup-rag-pipeline.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Clone repository
-git clone git@github.com:flyluk/rag_pipeline.git
-cd rag_pipeline
-
-# Create external volumes from docker-compose.yml
-if [ -f docker-compose.yml ]; then
-    grep -A 10 "^volumes:" docker-compose.yml | grep "external: true" -B 1 | grep -v "external: true" | grep -v "--" | sed 's/:$//' | sed 's/^[[:space:]]*//' | while read volume; do
-        [ -n "$volume" ] && docker volume create "$volume"
-    done
+# Clone or update repository
+if [ -d "rag_pipeline" ]; then
+    echo "Repository already exists, updating..."
+    cd rag_pipeline
+    git pull
+else
+    git clone git@github.com:flyluk/rag_pipeline.git
+    cd rag_pipeline
 fi
 
-# Run docker-compose
-docker-compose up -d
+# Create external volumes
+docker volume create open-webui
+docker volume create ollama
+docker volume create postgres_data
+docker volume create vllm_cache
+
+# Run docker compose with basic services
+export HF_TOKEN=""
+docker compose --profile enabled up -d
